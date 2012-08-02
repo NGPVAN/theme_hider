57b99a6595fb6928a9fc1d5dd4b5f7e0424d5c42
Victor Quinn <vquinn@ngpvan.com>
2012-08-02 16:47:07 -0400

    Initial commit of the Theme Hider module

---
+++ theme_hider.js
@@ -0,0 +1,14 @@
+(function($) {
+  Drupal.behaviors.theme_hider = function() {
+    $('#theme-hider-check-all').click(function() {
+      $('input[name^=theme_hider_hidden]').each(function() {
+        $(this).attr('checked', true);
+      });
+    });
+    $('#theme-hider-uncheck-all').click(function() {
+      $('input[name^=theme_hider_hidden]').each(function() {
+        $(this).attr('checked', false);
+      });
+    });
+  };  
+}(jQuery));
